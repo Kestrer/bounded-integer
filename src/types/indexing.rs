2ef6fef494616928079c6e3d02688daebcfdeb01
Kestrer <kestrer.dev@gmail.com>
2023-02-14 15:13:38 +0000

    Reorder `Index` implementations

--- src/types/indexing.rs
+++ src/types/indexing.rs
@@ -13,6 +13,13 @@ impl<const MIN: usize, const MAX: usize, T> Index<BoundedUsize<MIN, MAX>> for [T
     }
 }
 
+impl<const MIN: usize, const MAX: usize, T> IndexMut<BoundedUsize<MIN, MAX>> for [T] {
+    #[inline]
+    fn index_mut(&mut self, index: BoundedUsize<MIN, MAX>) -> &mut Self::Output {
+        &mut self[index.get()]
+    }
+}
+
 #[cfg(feature = "std")]
 impl<const MIN: usize, const MAX: usize, T> Index<BoundedUsize<MIN, MAX>> for std::vec::Vec<T> {
     type Output = T;
@@ -20,6 +27,14 @@ impl<const MIN: usize, const MAX: usize, T> Index<BoundedUsize<MIN, MAX>> for st
     #[inline]
     fn index(&self, index: BoundedUsize<MIN, MAX>) -> &Self::Output {
         &self[index.get()]
+    }
+}
+
+#[cfg(feature = "std")]
+impl<const MIN: usize, const MAX: usize, T> IndexMut<BoundedUsize<MIN, MAX>> for std::vec::Vec<T> {
+    #[inline]
+    fn index_mut(&mut self, index: BoundedUsize<MIN, MAX>) -> &mut Self::Output {
+        &mut self[index.get()]
     }
 }
 
@@ -35,21 +50,6 @@ impl<const MIN: usize, const MAX: usize, T> Index<BoundedUsize<MIN, MAX>>
     }
 }
 
-impl<const MIN: usize, const MAX: usize, T> IndexMut<BoundedUsize<MIN, MAX>> for [T] {
-    #[inline]
-    fn index_mut(&mut self, index: BoundedUsize<MIN, MAX>) -> &mut Self::Output {
-        &mut self[index.get()]
-    }
-}
-
-#[cfg(feature = "std")]
-impl<const MIN: usize, const MAX: usize, T> IndexMut<BoundedUsize<MIN, MAX>> for std::vec::Vec<T> {
-    #[inline]
-    fn index_mut(&mut self, index: BoundedUsize<MIN, MAX>) -> &mut Self::Output {
-        &mut self[index.get()]
-    }
-}
-
 #[cfg(feature = "std")]
 impl<const MIN: usize, const MAX: usize, T> IndexMut<BoundedUsize<MIN, MAX>>
     for std::collections::VecDeque<T>
@@ -60,19 +60,22 @@ impl<const MIN: usize, const MAX: usize, T> IndexMut<BoundedUsize<MIN, MAX>>
     }
 }
 
+#[cfg(test)]
 mod tests {
+    use crate::types::BoundedUsize;
+
     #[test]
     fn indexing() {
         let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
         for i in 0..arr.len() {
-            let b_u = crate::types::BoundedUsize::<0, 30>::new(i).unwrap();
+            let b_u = BoundedUsize::<0, 30>::new(i).unwrap();
             assert_eq!(arr[b_u], i);
         }
     }
 
-    #[cfg(feature = "std")]
     #[test]
+    #[cfg(feature = "std")]
     fn indexing_std() {
         let vec = (0..20).collect::<std::vec::Vec<usize>>();
         let deq = vec
@@ -82,7 +85,7 @@ mod tests {
             .collect::<std::collections::VecDeque<_>>();
 
         for i in 0..vec.len() {
-            let b_u = crate::types::BoundedUsize::<0, 30>::new(i).unwrap();
+            let b_u = BoundedUsize::<0, 30>::new(i).unwrap();
 
             assert_eq!(vec[b_u], i);
             assert_eq!(deq[b_u], 19 - i);
@@ -94,7 +97,7 @@ mod tests {
         let mut arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
         for i in 0..arr.len() {
-            let b_u = crate::types::BoundedUsize::<0, 30>::new(i).unwrap();
+            let b_u = BoundedUsize::<0, 30>::new(i).unwrap();
 
             arr[b_u] += 5;
 
@@ -102,8 +105,8 @@ mod tests {
         }
     }
 
-    #[cfg(feature = "std")]
     #[test]
+    #[cfg(feature = "std")]
     fn indexing_mut_std() {
         let mut vec = (0..20).collect::<std::vec::Vec<usize>>();
         let mut deq = vec
@@ -113,7 +116,7 @@ mod tests {
             .collect::<std::collections::VecDeque<_>>();
 
         for i in 0..vec.len() {
-            let b_u = crate::types::BoundedUsize::<0, 30>::new(i).unwrap();
+            let b_u = BoundedUsize::<0, 30>::new(i).unwrap();
 
             vec[b_u] += 5;
             deq[b_u] += 10;
