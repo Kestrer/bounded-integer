c6e2ead4123d716d6c5dfcdbeae5fadacad5ced0
Oxey <44652133+O-X-E-Y@users.noreply.github.com>
2023-02-14 15:57:10 +0100

    Add std::ops::Index on Vec, VecDeque and arrays for BoundedUsize (#17)
    * add `std::ops::Index` impl for BoundedUsize
    
    * change test `mod` name to `usize2`
    
    * ran `cargo fmt`
    
    * make `Index` work on `[T]` rather than `[T; N]`
    
    * use correct feature flag + use `core::ops`
    
    * add `core::ops::IndexMut` for BoundedUsize
    
    * fix tests for index, indexmut with `std` variants

--- src/types.rs
+++ src/types.rs
@@ -1388,3 +1388,124 @@ define_bounded_integers! {
     BoundedI64 i64 signed -> i64,
     BoundedIsize isize signed -> isize,
 }
+
+//Indexing operations on [T; N], Vec<T> and VecDeque<T> for BoundedUsize
+impl<const MIN: usize, const MAX: usize, T> core::ops::Index<BoundedUsize<MIN, MAX>> for [T] {
+    type Output = T;
+
+    #[inline]
+    fn index(&self, index: BoundedUsize<MIN, MAX>) -> &Self::Output {
+        &self[index.get()]
+    }
+}
+#[cfg(feature = "std")]
+impl<const MIN: usize, const MAX: usize, T> core::ops::Index<BoundedUsize<MIN, MAX>>
+    for std::vec::Vec<T>
+{
+    type Output = T;
+
+    #[inline]
+    fn index(&self, index: BoundedUsize<MIN, MAX>) -> &Self::Output {
+        &self[index.get()]
+    }
+}
+#[cfg(feature = "std")]
+impl<const MIN: usize, const MAX: usize, T> core::ops::Index<BoundedUsize<MIN, MAX>>
+    for std::collections::VecDeque<T>
+{
+    type Output = T;
+
+    #[inline]
+    fn index(&self, index: BoundedUsize<MIN, MAX>) -> &Self::Output {
+        &self[index.get()]
+    }
+}
+
+impl<const MIN: usize, const MAX: usize, T> core::ops::IndexMut<BoundedUsize<MIN, MAX>> for [T] {
+    #[inline]
+    fn index_mut(&mut self, index: BoundedUsize<MIN, MAX>) -> &mut Self::Output {
+        &mut self[index.get()]
+    }
+}
+#[cfg(feature = "std")]
+impl<const MIN: usize, const MAX: usize, T> core::ops::IndexMut<BoundedUsize<MIN, MAX>>
+    for std::vec::Vec<T>
+{
+    #[inline]
+    fn index_mut(&mut self, index: BoundedUsize<MIN, MAX>) -> &mut Self::Output {
+        &mut self[index.get()]
+    }
+}
+#[cfg(feature = "std")]
+impl<const MIN: usize, const MAX: usize, T> core::ops::IndexMut<BoundedUsize<MIN, MAX>>
+    for std::collections::VecDeque<T>
+{
+    #[inline]
+    fn index_mut(&mut self, index: BoundedUsize<MIN, MAX>) -> &mut Self::Output {
+        &mut self[index.get()]
+    }
+}
+
+mod indexing_tests {
+    #[test]
+    fn indexing() {
+        let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        for i in 0..arr.len() {
+            let b_u = crate::types::BoundedUsize::<0, 30>::new(i).unwrap();
+            assert_eq!(arr[b_u], i);
+        }
+    }
+
+    #[cfg(feature = "std")]
+    #[test]
+    fn indexing_std() {
+        let vec = (0..20).collect::<std::vec::Vec<usize>>();
+        let deq = vec
+            .clone()
+            .into_iter()
+            .rev()
+            .collect::<std::collections::VecDeque<_>>();
+
+        for i in 0..vec.len() {
+            let b_u = crate::types::BoundedUsize::<0, 30>::new(i).unwrap();
+
+            assert_eq!(vec[b_u], i);
+            assert_eq!(deq[b_u], 19 - i);
+        }
+    }
+
+    #[test]
+    fn indexing_mut() {
+        let mut arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        for i in 0..arr.len() {
+            let b_u = crate::types::BoundedUsize::<0, 30>::new(i).unwrap();
+
+            arr[b_u] += 5;
+
+            assert_eq!(arr[b_u], i + 5);
+        }
+    }
+
+    #[cfg(feature = "std")]
+    #[test]
+    fn indexing_mut_std() {
+        let mut vec = (0..20).collect::<std::vec::Vec<usize>>();
+        let mut deq = vec
+            .clone()
+            .into_iter()
+            .rev()
+            .collect::<std::collections::VecDeque<_>>();
+
+        for i in 0..vec.len() {
+            let b_u = crate::types::BoundedUsize::<0, 30>::new(i).unwrap();
+
+            vec[b_u] += 5;
+            deq[b_u] += 10;
+
+            assert_eq!(vec[b_u], i + 5);
+            assert_eq!(deq[b_u], (19 - i) + 10);
+        }
+    }
+}
