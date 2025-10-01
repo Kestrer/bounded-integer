01d7474b3922acb32b52ea0ecbed298734646182
Eira Fransham <eira@fourieraudio.com>
2025-10-01 11:43:14 +0200

    Clean up tests based on review

--- optimization-tests/src/lib.rs
+++ optimization-tests/src/lib.rs
@@ -1,9 +1,5 @@
 //! Checking compilation with optimizations for the `assert_unchecked` range tests in `unsafe_api`.
 //!
-//! > *TODO*: Rust seems to ignore optimization for doctests even if they are specified in the profile,
-//! > and even if running `cargo test --doc --release`. The `compile_fail` test _does_ correctly fail
-//! > to compile, but that is probably true even for out-of-range comparisons.
-//!
 //! ```rust,compile_fail
 //! const LOWER_BOUND: usize = 15;
 //!
@@ -21,40 +17,48 @@
 //! )
 //! ```
 
+// We should not export anything when not running tests
+#![cfg(test)]
+
+use bounded_integer::BoundedUsize;
+
 unsafe extern "C" {
-    // This function should fail to link if range checks are not optimized out as expected.
-    pub safe fn should_be_optimized_out() -> !;
+    /// This function should fail to link if range checks are not optimized out as expected.
+    safe fn should_be_optimized_out() -> !;
 }
 
+/// Ensure that LLVM has enough information at compile-time to statically ensure that the
+/// condition is true. If LLVM cannot statically ensure that the condition is true and
+/// emits a run-time branch, the binary will contain a call to a non-existent `extern`
+/// function and fail to link.
 #[macro_export]
-macro_rules! assert_optimized_out {
+macro_rules! const_assert {
     ($cond:expr) => {
-        if $cond {
+        if !$cond {
             $crate::should_be_optimized_out();
         }
-    }
+    };
 }
 
-#[cfg(test)]
-mod tests {
-    use bounded_integer::BoundedUsize;
-    use crate::assert_optimized_out;
+/// Assert that the inner value is statically enforced to be between the bounds `LO` and
+/// `HI` inclusive.
+fn range_check_optimized_out_usize<const LO: usize, const HI: usize>(expected: usize) {
+    let i = core::hint::black_box(BoundedUsize::<LO, HI>::new(expected).unwrap());
+    const_assert!(i.get() >= LO && i.get() <= HI);
+    let i = core::hint::black_box(i);
+    const_assert!(*i.get_ref() >= LO && *i.get_ref() <= HI);
+    let mut i = core::hint::black_box(i);
+    const_assert!(*unsafe { i.get_mut() } >= LO && *unsafe { i.get_mut() } <= HI);
 
-    fn range_check_optimized_out_usize<const LO: usize, const HI: usize>(expected: usize) {
-        let i = core::hint::black_box(BoundedUsize::<LO, HI>::new(expected).unwrap());
-        assert_optimized_out!(i.get() < LO || i.get() > HI);
-        let i = core::hint::black_box(i);
-        assert_optimized_out!(*i.get_ref() < LO || i.get() > HI);
-        let mut i = core::hint::black_box(i);
-        assert_optimized_out!(*unsafe { i.get_mut() } < LO || *unsafe { i.get_mut() } > HI);
+    assert_eq!(
+        core::hint::black_box(i.get()),
+        core::hint::black_box(expected)
+    );
+}
 
-        assert_eq!(core::hint::black_box(i.get()), core::hint::black_box(expected));
-    }
-
-    #[test]
-    fn range_check_optimized_out() {
-        range_check_optimized_out_usize::<10, 20>(15);
-        range_check_optimized_out_usize::<20, 20>(20);
-        range_check_optimized_out_usize::<1, { usize::MAX - 1 }>(usize::MAX - 1);
-    }
+#[test]
+fn range_check_optimized_out() {
+    range_check_optimized_out_usize::<10, 20>(15);
+    range_check_optimized_out_usize::<20, 20>(20);
+    range_check_optimized_out_usize::<1, { usize::MAX - 1 }>(usize::MAX - 1);
 }
